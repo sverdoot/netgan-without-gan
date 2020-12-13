a12f55abdba172f9ccb624c11760ab1da54b2e5a
sverdoot <lagutin.em@phystech.edu>
2020-12-13 13:39:13 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(name='netgan_without_gan', 
+      version='0.1',
+      description='NetGAN without GAN: From Random Walks to Low-Rank Approximations', 
+      packages=find_packages())
