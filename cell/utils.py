4c3a2c4762b8814160945076984da0d487a2115c
sverdoot <lagutin.em@phystech.edu>
2020-12-14 13:32:36 +0300

    add some code

---
+++ cell/utils.py
@@ -0,0 +1,414 @@
+import numpy as np
+import scipy.sparse as sp
+from scipy.sparse.csgraph import minimum_spanning_tree, connected_components
+from scipy.sparse.linalg import eigs
+from sklearn.metrics import roc_auc_score, average_precision_score
+import networkx as nx
+
+
+def load_npz(file_name):
+    """Load a SparseGraph from a Numpy binary file.
+    Parameters
+    ----------
+    file_name : str
+        Name of the file to load.
+    Returns
+    -------
+    sparse_graph : gust.SparseGraph
+        Graph in sparse matrix format.
+    """
+    if not file_name.endswith('.npz'):
+        file_name += '.npz'
+    with np.load(file_name, allow_pickle=True) as loader:
+        loader = dict(loader)
+        adj_matrix = sp.csr_matrix((loader['adj_data'], loader['adj_indices'],
+                                    loader['adj_indptr']), shape=loader['adj_shape'])
+
+        if 'attr_data' in loader:
+            attr_matrix = sp.csr_matrix((loader['attr_data'], loader['attr_indices'],
+                                         loader['attr_indptr']), shape=loader['attr_shape'])
+        else:
+            attr_matrix = None
+
+        labels = loader.get('labels')
+
+    return adj_matrix, attr_matrix, labels
+
+
+# def load_npz(file_name):
+#     """Load a SparseGraph from a Numpy binary file.
+#     Parameters
+#     ----------
+#     file_name : str
+#         Name of the file to load.
+#     Returns
+#     -------
+#     sparse_graph : SparseGraph
+#         Graph in sparse matrix format.
+#     """
+#     if not file_name.endswith('.npz'):
+#         file_name += '.npz'
+#     with np.load(file_name, allow_pickle=True) as loader:
+#         loader = dict(loader)['arr_0'].item()
+#         adj_matrix = sp.csr_matrix((loader['adj_data'], loader['adj_indices'],
+#                                               loader['adj_indptr']), shape=loader['adj_shape'])
+
+#         if 'attr_data' in loader:
+#             attr_matrix = sp.csr_matrix((loader['attr_data'], loader['attr_indices'],
+#                                                    loader['attr_indptr']), shape=loader['attr_shape'])
+#         else:
+#             attr_matrix = None
+
+#         labels = loader.get('labels')
+
+#     return adj_matrix, attr_matrix, labels
+
+
+def largest_connected_components(adj, n_components=1):
+    """Select the largest connected components in the graph.
+    Parameters
+    ----------
+    sparse_graph : SparseGraph
+        Input graph.
+    n_components : int, default 1
+        Number of largest connected components to keep.
+    Returns
+    -------
+    sparse_graph : SparseGraph
+        Subgraph of the input graph where only the nodes in largest n_components are kept.
+    """
+    _, component_indices = connected_components(adj)
+    component_sizes = np.bincount(component_indices)
+    components_to_keep = np.argsort(component_sizes)[::-1][:n_components]  # reverse order to sort descending
+    nodes_to_keep = [
+        idx for (idx, component) in enumerate(component_indices) if component in components_to_keep
+
+
+    ]
+    print("Selecting {0} largest connected components".format(n_components))
+    return nodes_to_keep
+
+
+def edges_to_sparse(edges, N, values=None):
+    """
+    Create a sparse adjacency matrix from an array of edge indices and (optionally) values.
+    Parameters
+    ----------
+    edges : array-like, shape [n_edges, 2]
+        Edge indices
+    N : int
+        Number of nodes
+    values : array_like, shape [n_edges]
+        The values to put at the specified edge indices. Optional, default: np.ones(.)
+    Returns
+    -------
+    A : scipy.sparse.csr.csr_matrix
+        Sparse adjacency matrix
+    """
+    if values is None:
+        values = np.ones(edges.shape[0])
+
+    return sp.coo_matrix((values, (edges[:, 0], edges[:, 1])), shape=(N, N)).tocsr()
+
+
+def train_val_test_split_adjacency(A, p_val=0.10, p_test=0.05, seed=0, neg_mul=1,
+                                   every_node=True, connected=False, undirected=False,
+                                   use_edge_cover=True, set_ops=True, asserts=False):
+    """
+    Split the edges of the adjacency matrix into train, validation and test edges
+    and randomly samples equal amount of validation and test non-edges.
+    Parameters
+    ----------
+    A : scipy.sparse.spmatrix
+        Sparse unweighted adjacency matrix
+    p_val : float
+        Percentage of validation edges. Default p_val=0.10
+    p_test : float
+        Percentage of test edges. Default p_test=0.05
+    seed : int
+        Seed for numpy.random. Default seed=0
+    neg_mul : int
+        What multiplicity of negative samples (non-edges) to have in the test/validation set
+        w.r.t the number of edges, i.e. len(non-edges) = L * len(edges). Default neg_mul=1
+    every_node : bool
+        Make sure each node appears at least once in the train set. Default every_node=True
+    connected : bool
+        Make sure the training graph is still connected after the split
+    undirected : bool
+        Whether to make the split undirected, that is if (i, j) is in val/test set then (j, i) is there as well.
+        Default undirected=False
+    use_edge_cover: bool
+        Whether to use (approximate) edge_cover to find the minimum set of edges that cover every node.
+        Only active when every_node=True. Default use_edge_cover=True
+    set_ops : bool
+        Whether to use set operations to construction the test zeros. Default setwise_zeros=True
+        Otherwise use a while loop.
+    asserts : bool
+        Unit test like checks. Default asserts=False
+    Returns
+    -------
+    train_ones : array-like, shape [n_train, 2]
+        Indices of the train edges
+    val_ones : array-like, shape [n_val, 2]
+        Indices of the validation edges
+    val_zeros : array-like, shape [n_val, 2]
+        Indices of the validation non-edges
+    test_ones : array-like, shape [n_test, 2]
+        Indices of the test edges
+    test_zeros : array-like, shape [n_test, 2]
+        Indices of the test non-edges
+    """
+    assert p_val + p_test > 0
+    assert A.max() == 1  # no weights
+    assert A.min() == 0  # no negative edges
+    #assert A.diagonal().sum() == 0  # no self-loops
+    #assert not np.any(A.sum(0).A1 + A.sum(1).A1 == 0)  # no dangling nodes
+
+    is_undirected = (A != A.T).nnz == 0
+
+    if undirected:
+        assert is_undirected  # make sure is directed
+        A = sp.tril(A).tocsr()  # consider only upper triangular
+        A.eliminate_zeros()
+    else:
+        if is_undirected:
+            warnings.warn('Graph appears to be undirected. Did you forgot to set undirected=True?')
+
+    np.random.seed(seed)
+
+    E = A.nnz
+    N = A.shape[0]
+    s_train = int(E * (1 - p_val - p_test))
+
+    idx = np.arange(N)
+
+    # hold some edges so each node appears at least once
+    if every_node:
+        if connected:
+            assert connected_components(A)[0] == 1  # make sure original graph is connected
+            A_hold = minimum_spanning_tree(A)
+        else:
+            A.eliminate_zeros()  # makes sure A.tolil().rows contains only indices of non-zero elements
+            d = A.sum(1).A1
+
+            if use_edge_cover:
+                hold_edges = np.array(list(nx.maximal_matching(nx.DiGraph(A))))
+                not_in_cover = np.array(list(set(range(N)).difference(hold_edges.flatten())))
+
+                # makes sure the training percentage is not smaller than N/E when every_node is set to True
+                min_size = hold_edges.shape[0] + len(not_in_cover)
+                if min_size > s_train:
+                    raise ValueError('Training percentage too low to guarantee every node. Min train size needed {:.2f}'
+                                     .format(min_size / E))
+
+                d_nic = d[not_in_cover]
+
+                hold_edges_d1 = np.column_stack((not_in_cover[d_nic > 0],
+                                                 np.row_stack(map(np.random.choice,
+                                                                  A[not_in_cover[d_nic > 0]].tolil().rows))))
+
+                if np.any(d_nic == 0):
+                    hold_edges_d0 = np.column_stack((np.row_stack(map(np.random.choice, A[:, not_in_cover[d_nic == 0]].T.tolil().rows)),
+                                                     not_in_cover[d_nic == 0]))
+                    hold_edges = np.row_stack((hold_edges, hold_edges_d0, hold_edges_d1))
+                else:
+                    hold_edges = np.row_stack((hold_edges, hold_edges_d1))
+
+            else:
+                # makes sure the training percentage is not smaller than N/E when every_node is set to True
+                if N > s_train:
+                    raise ValueError('Training percentage too low to guarantee every node. Min train size needed {:.2f}'
+                                     .format(N / E))
+
+                hold_edges_d1 = np.column_stack(
+                    (idx[d > 0], np.row_stack(map(np.random.choice, A[d > 0].tolil().rows))))
+
+                if np.any(d == 0):
+                    hold_edges_d0 = np.column_stack((np.row_stack(map(np.random.choice, A[:, d == 0].T.tolil().rows)),
+                                                     idx[d == 0]))
+                    hold_edges = np.row_stack((hold_edges_d0, hold_edges_d1))
+                else:
+                    hold_edges = hold_edges_d1
+
+            if asserts:
+                assert np.all(A[hold_edges[:, 0], hold_edges[:, 1]])
+                assert len(np.unique(hold_edges.flatten())) == N
+
+            A_hold = edges_to_sparse(hold_edges, N)
+
+        A_hold[A_hold > 1] = 1
+        A_hold.eliminate_zeros()
+        A_sample = A - A_hold
+
+        s_train = s_train - A_hold.nnz
+    else:
+        A_sample = A
+
+    idx_ones = np.random.permutation(A_sample.nnz)
+    ones = np.column_stack(A_sample.nonzero())
+    train_ones = ones[idx_ones[:s_train]]
+    test_ones = ones[idx_ones[s_train:]]
+
+    # return back the held edges
+    if every_node:
+        train_ones = np.row_stack((train_ones, np.column_stack(A_hold.nonzero())))
+
+    n_test = len(test_ones) * neg_mul
+    if set_ops:
+        # generate slightly more completely random non-edge indices than needed and discard any that hit an edge
+        # much faster compared a while loop
+        # in the future: estimate the multiplicity (currently fixed 1.3/2.3) based on A_obs.nnz
+        if undirected:
+            random_sample = np.random.randint(0, N, [int(2.3 * n_test), 2])
+            random_sample = random_sample[random_sample[:, 0] > random_sample[:, 1]]
+        else:
+            random_sample = np.random.randint(0, N, [int(1.3 * n_test), 2])
+            random_sample = random_sample[random_sample[:, 0] != random_sample[:, 1]]
+
+        test_zeros = random_sample[A[random_sample[:, 0], random_sample[:, 1]].A1 == 0]
+        test_zeros = np.row_stack(test_zeros)[:n_test]
+        assert test_zeros.shape[0] == n_test
+    else:
+        test_zeros = []
+        while len(test_zeros) < n_test:
+            i, j = np.random.randint(0, N, 2)
+            if A[i, j] == 0 and (not undirected or i > j) and (i, j) not in test_zeros:
+                test_zeros.append((i, j))
+        test_zeros = np.array(test_zeros)
+
+    # split the test set into validation and test set
+    s_val_ones = int(len(test_ones) * p_val / (p_val + p_test))
+    s_val_zeros = int(len(test_zeros) * p_val / (p_val + p_test))
+
+    val_ones = test_ones[:s_val_ones]
+    test_ones = test_ones[s_val_ones:]
+
+    val_zeros = test_zeros[:s_val_zeros]
+    test_zeros = test_zeros[s_val_zeros:]
+
+    if undirected:
+        # put (j, i) edges for every (i, j) edge in the respective sets and form back original A
+        symmetrize = lambda x: np.row_stack((x, np.column_stack((x[:, 1], x[:, 0]))))
+        train_ones = symmetrize(train_ones)
+        val_ones = symmetrize(val_ones)
+        val_zeros = symmetrize(val_zeros)
+        test_ones = symmetrize(test_ones)
+        test_zeros = symmetrize(test_zeros)
+        A = A.maximum(A.T)
+
+    if asserts:
+        set_of_train_ones = set(map(tuple, train_ones))
+        assert train_ones.shape[0] + test_ones.shape[0] + val_ones.shape[0] == A.nnz
+        assert (edges_to_sparse(np.row_stack((train_ones, test_ones, val_ones)), N) != A).nnz == 0
+        assert set_of_train_ones.intersection(set(map(tuple, test_ones))) == set()
+        assert set_of_train_ones.intersection(set(map(tuple, val_ones))) == set()
+        assert set_of_train_ones.intersection(set(map(tuple, test_zeros))) == set()
+        assert set_of_train_ones.intersection(set(map(tuple, val_zeros))) == set()
+        assert len(set(map(tuple, test_zeros))) == len(test_ones) * neg_mul
+        assert len(set(map(tuple, val_zeros))) == len(val_ones) * neg_mul
+        assert not connected or connected_components(A_hold)[0] == 1
+        assert not every_node or ((A_hold - A) > 0).sum() == 0
+
+    return train_ones, val_ones, val_zeros, test_ones, test_zeros
+
+
+def edge_overlap(A, B):
+    """
+    Compute edge overlap between two graphs (amount of shared edges).
+    Args:
+        A (sp.csr.csr_matrix): First input adjacency matrix.
+        B (sp.csr.csr_matrix): Second input adjacency matrix.
+    Returns:
+        Edge overlap.
+    """
+
+    return A.multiply(B).sum() / 2
+
+
+def link_prediction_performance(scores_matrix, val_ones, val_zeros):
+    """
+    Compute the link prediction performance of a score matrix on a set of validation edges and non-edges.
+    Args:
+        scores_matrix (np.array): Symmetric scores matrix of the graph generative model.
+        val_ones (np.array): Validation edges. Rows represent indices of the input adjacency matrix with value 1. 
+        val_zeros (np.array): Validation non-edges. Rows represent indices of the input adjacency matrix with value 0.
+        
+    Returns:
+       2-Tuple containg ROC-AUC score and Average precision.
+    """
+
+    actual_labels_val = np.append(np.ones(len(val_ones)), np.zeros(len(val_zeros)))
+    edge_scores = np.append(
+        scores_matrix[val_ones[:, 0], val_ones[:, 1]],
+        scores_matrix[val_zeros[:, 0], val_zeros[:, 1]],
+    )
+    return (
+        roc_auc_score(actual_labels_val, edge_scores),
+        average_precision_score(actual_labels_val, edge_scores),
+    )
+
+
+def scores_matrix_from_transition_matrix(transition_matrix, symmetric=True):
+    """
+    Compute the scores matrix from the transition matrix.
+    Args:
+        transition_matrix (np.array, shape=(N,N)): Matrix whose entries (i,j) correspond to the probability of a 
+                                                   transition from node i to j.
+        symmetric (bool, default:True): If True, symmetrize the resulting scores matrix.
+    Returns:
+        scores_matrix(sp.csr.csr_matrix, shape=(N, N)): Matrix whose entries (i,j) correspond to the weight of the 
+                                                        directed edge (i, j) in an edge-independent model.
+    """
+
+    N = transition_matrix.shape[0]
+    p_stationary = np.real(eigs(transition_matrix.T, k=1, sigma=0.99999)[1])
+    p_stationary /= p_stationary.sum()
+    scores_matrix = np.maximum(p_stationary * transition_matrix, 0)
+
+    if symmetric:
+        scores_matrix += scores_matrix.T
+
+    return scores_matrix
+
+
+def graph_from_scores(scores_matrix, n_edges):
+    """
+    Assemble a symmetric binary graph from the input score matrix. Ensures that there will be no singleton nodes.
+    See the paper for details.
+    Args:
+        scores_matrix (sp.csr.csr_matrix, shape=(N, N)): Matrix whose entries (i,j) correspond to the weight of the 
+                                                        directed edge (i, j) in an edge-independent model.
+        n_edges (int): The desired number of edges in the generated graph.
+    Returns
+    -------
+    target_g (sp.csr.csr_matrix, shape=(N, N)): Adjacency matrix of the generated graph.
+    """
+
+    target_g = sp.csr_matrix(scores_matrix.shape)
+
+    np.fill_diagonal(scores_matrix, 0)
+
+    degrees = scores_matrix.sum(1)  # The row sum over the scores_matrix.
+
+    N = scores_matrix.shape[0]
+
+    for n in range(N):  # Iterate over the nodes
+        target = np.random.choice(N, p=scores_matrix[n] / degrees[n])
+        target_g[n, target] = 1
+        target_g[target, n] = 1
+
+    diff = np.round((2 * n_edges - target_g.sum()) / 2)
+    if diff > 0:
+        triu = np.triu(scores_matrix)
+        triu[target_g.nonzero()] = 0
+        triu = triu / triu.sum()
+
+        triu_ixs = np.triu_indices_from(scores_matrix)
+        extra_edges = np.random.choice(
+            triu_ixs[0].shape[0], replace=False, p=triu[triu_ixs], size=int(diff)
+        )
+
+        target_g[(triu_ixs[0][extra_edges], triu_ixs[1][extra_edges])] = 1
+        target_g[(triu_ixs[1][extra_edges], triu_ixs[0][extra_edges])] = 1
+
+    return target_g
