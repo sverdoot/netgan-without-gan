16849ed8acf502012ae10879ef67e79458ae6b87
Pavel <pavel.burnysheff@yandex.ru>
2020-12-15 00:37:43 +0300

    update_graphic

--- stat_during_training.py
+++ stat_during_training.py
@@ -16,13 +16,15 @@ EDGE_OVERLAP_LIMIT = {
     'citeseer' : 0.8,
     'polblogs': 0.7
 }
+MAX_STEPS = 150
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', type=str, default='../data/cora_ml.npz')
     parser.add_argument('--data_name', type=str, default='cora_ml')
     parser.add_argument('--statistics_step', type=int, default=10)
     parser.add_argument('--number_of_samples', type=int, default=5)
-    parser.add_argument('--g_type', type=str, default='cell')
+    parser.add_argument('--g_type', type=str, default='all')
     parser.add_argument('--lr', type=float, default=0.1)
     parser.add_argument('--weight_decay', type=float, default=1e-6)
     parser.add_argument('--save_path', type=str, default='img/stat.png')
@@ -38,27 +40,39 @@ def main(args):
     _A_obs = _A_obs[lcc,:][:,lcc]
     _N = _A_obs.shape[0]
 
-    model = Cell(A=_A_obs, H=9, g_type=args.g_type, callbacks=[EdgeOverlapCriterion(invoke_every=10, edge_overlap_limit=.7)]) 
-    optimizer_args = {'lr': args.lr, 'weight_decay': args.weight_decay} 
-    model._optimizer  = torch.optim.Adam(model.g.parameters(), **optimizer_args)    
     if args.data_name in EDGE_OVERLAP_LIMIT.keys():
         edge_overlap_limit = EDGE_OVERLAP_LIMIT[args.data_name]
     else:
         edge_overlap_limit = 0.6
 
-    iter = 0
-    current_overlap = 0.0  
-    training_stat = list()
-    while current_overlap < edge_overlap_limit:
-        iter += 1
-        loss, time = model._train_step()
-        model.update_scores_matrix()
-        if iter % args.statistics_step == 0:
-            generated_graphs = [model.sample_graph() for _ in range(args.number_of_samples)]
-            current_overlap = np.mean([edge_overlap(model.A_sparse, gg) / model.num_edges for gg in generated_graphs])
-            stats = [compute_graph_statistics(gg) for gg in generated_graphs]
-            training_stat.append({'overlap': current_overlap, 'stats': stats})
-            print(f"Iteration: {iter}; loss: {loss}; overlap: {current_overlap}")
+    training_stat = dict()
+    
+    
+    if args.g_type != 'all':
+        training_stat[args.g_type] = list()
+    else:
+        training_stat = {i: list() for i in ['cell', 'fc', 'svd']}
+    
+    for model_name in list(training_stat.keys()):    
+        print(f"'{model_name}' Approach")
+        model = Cell(A=_A_obs, H=9, g_type=model_name) 
+        optimizer_args = {'lr': args.lr, 'weight_decay': args.weight_decay} 
+        model._optimizer  = torch.optim.Adam(model.g.parameters(), **optimizer_args)    
+
+        iter = 0
+        current_overlap = 0.0  
+
+        while current_overlap < edge_overlap_limit and iter < MAX_STEPS:
+            iter += 1
+            loss, time = model._train_step()
+            model.update_scores_matrix()
+            if iter % args.statistics_step == 0:
+                generated_graphs = [model.sample_graph() for _ in range(args.number_of_samples)]
+                current_overlap = np.mean([edge_overlap(model.A_sparse, gg) / model.num_edges for gg in generated_graphs])
+                stats = [compute_graph_statistics(gg) for gg in generated_graphs]
+                training_stat[model_name].append({'overlap': current_overlap, 'stats': stats})
+                print(f"Iteration: {iter}; loss: {loss}; overlap: {current_overlap}")
+
 
     original_stat = compute_graph_statistics(model.A_sparse)
 
@@ -73,13 +87,14 @@ def main(args):
         axs[stat_id // 3, stat_id % 3].set_ylabel(stat_name, fontsize=18)
         axs[stat_id // 3, stat_id % 3].set_xlabel('Edge overlap (in %)', fontsize=13)
         axs[stat_id // 3, stat_id % 3].axhline(y=original_stat[stat], color='g', linestyle='--', label='target')
-        axs[stat_id // 3, stat_id % 3].errorbar([100 * i['overlap'] for i in training_stat], 
-                                                [np.mean([j[stat] for j in i['stats']]) for i in training_stat],
-                                                [np.std([j[stat] for j in i['stats']]) for i in training_stat],
-                                                ls='none',
-                                                fmt='.',
-                                                label=args.g_type
-                                                )
+        for model_name, model_statistic in training_stat.items():
+            axs[stat_id // 3, stat_id % 3].errorbar([100 * i['overlap'] for i in model_statistic], 
+                                                    [np.mean([j[stat] for j in i['stats']]) for i in model_statistic],
+                                                    [np.std([j[stat] for j in i['stats']]) for i in model_statistic],
+                                                    ls='none',
+                                                    fmt='.',
+                                                    label=model_name
+                                                    )
 
     axLine, axLabel = axs[stat_id // 3, stat_id % 3].get_legend_handles_labels()
     fig.legend(axLine, axLabel, loc = 'center right', fontsize=15)
@@ -87,7 +102,6 @@ def main(args):
     if args.save_path is not None:
         plt.savefig(args.save_path)
     plt.close()
-
 
 
 if __name__ == '__main__':
